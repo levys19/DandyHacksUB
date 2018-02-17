e6c8b20d06f5ffa75a7a0e3f9abd2bf73457db1a
jamesjhuang <jianguoh@buffalo.edu>
2018-02-17 17:50:22 -0500

    graph.html and pieplot in index.py

--- index.py
+++ index.py
@@ -2,7 +2,19 @@ from flask import Flask, redirect, render_template, request, url_for
 import json, requests
 from twit import twit
 
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly
+
 app = Flask(__name__)
+
+def plotpie():
+    labels = ['Positive', 'Negative', 'Neutral']
+    values = [18, 7, 5]
+    colors = ['#87F971', '#F24D4D', '#70ABEA']
+    trace = go.Pie(labels=labels, values=values)
+    layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
+    return plotly.offline.plot(go.Figure(data=[trace], layout=layout), output_type="div")
 
 @app.route('/', methods=['GET'])
 def index():
@@ -11,6 +23,8 @@ def index():
 @app.route('/graph', methods=['POST'])
 def graph():
     #grabs the keyword from the form
-    keyword = request.form['key_word']
-    searchTwit = twit()
-    twit.search(keyword)
+    # keyword = request.form['key_word']
+    # searchTwit = twit()
+    # twit.search(keyword)
+    return render_template('graph.html', chart=plotpie())
+
