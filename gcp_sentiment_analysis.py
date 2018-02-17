9b79f61a538a239904541a9a64749b433a572a46
Jimmy Huang <jhuang42@buffalo.edu>
2018-02-17 11:55:54 -0500

    Basic sentiment analysis referencing the sentiment tutorial from GCP

---
+++ gcp_sentiment_analysis.py
@@ -0,0 +1,45 @@
+import argparse
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+def print_result(annotations):
+	score = annotations.document_sentiment.score
+	magnitude = annotations.document_sentiment.magnitude
+
+	for index, sentence in enumerate(annotations.sentences):
+		sentence_sentiment = sentence.sentiment.score
+		print('Sentence {} has a sentiment score of {}'.format(
+			index, sentence_sentiment))
+	
+	print('Overall Sentiment: score of {} with magnitude of {}'.format(
+		score, magnitude))
+	return 0
+
+def analyze(filename):
+	"""Run a sentiment analysis request on text within a passed filename."""
+	client = language.LanguageServiceClient()
+
+	with open(filename, 'r') as readfile:
+		#Instantiate a plain text document
+		content = readfile.read()
+
+	document = types.Document(
+			content = content,
+			type=enums.Document.Type.PLAIN_TEXT)
+	annotations = client.analyze_sentiment(document=document)
+
+	#Print results
+	print_result(annotations)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(
+			description=__doc__,
+			formatter_class=argparse.RawDescriptionHelpFormatter)
+	parser.add_argument(
+			'filename',
+			help='The filename of the text you\'d like to analyze.')
+	args=parser.parse_args()
+
+	analyze(args.filename)
