1b2f190a855763ee2cb45e5508bfae032b98d474
Levy Shi <levyshi@buffalo.edu>
2018-02-18 09:41:27 -0500

    wowwo

--- index.py
+++ index.py
@@ -12,18 +12,10 @@ import plotly
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-def plotpie():
-    labels = ['Positive', 'Negative', 'Neutral']
-    values = [18, 7, 5]
-    colors = ['#87F971', '#F24D4D', '#70ABEA']
-    trace = go.Pie(labels=labels, values=values)
-=======
 def plotpie(values):
     labels = ['Positive', 'Negative', 'Neutral']
     colors = ['#87F971', '#F24D4D', '#70ABEA']
     trace = go.Pie(labels=labels, values=values, marker =dict(colors=colors))
->>>>>>> 610ddb42fe9aecaf522c52d7afc0a05d170b246a
     layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
     return plotly.offline.plot(go.Figure(data=[trace], layout=layout), output_type="div")
 
@@ -33,13 +25,6 @@ def index():
 
 @app.route('/graph', methods=['POST'])
 def graph():
-<<<<<<< HEAD
-    #grabs the keyword from the form
-    # keyword = request.form['key_word']
-    # searchTwit = twit()
-    # twit.search(keyword)
-    return render_template('graph.html', chart=plotpie())
-=======
     # grabs the keyword from the form
     keyword = request.form['key_word']
     searchTwit = twit()
@@ -47,6 +32,4 @@ def graph():
     results = twit.search()
     makeWordCount = twit.wordCount()
     sentence = NLTK_POS.makeSentence(twit.frequency(makeWordCount), keyword)
-
     return render_template('graph.html', chart = plotpie(results), sentence = sentence, dictionary=makeWordCount)
->>>>>>> 610ddb42fe9aecaf522c52d7afc0a05d170b246a
